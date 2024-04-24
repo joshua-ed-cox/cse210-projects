4f64e33d1b4fcc77c01cc7fc73aa2969f1a35c2d
joshua-ed-cox <jec.professions@gmail.com>
2024-04-24 03:07:39 -0600

    added writeline and write statements

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+        Console.WriteLine("You print in C# by using the Console.WriteLine function.");
+        Console.Write("There will not be a newline after this. (Console.Write)");
     }
 }
