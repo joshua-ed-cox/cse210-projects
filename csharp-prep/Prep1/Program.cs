f591f38919b5f3adf2832fa6865073b3653649e1
joshua-ed-cox <jec.professions@gmail.com>
2024-04-24 03:19:54 -0600

    completed prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name: ");
+        string first_nam = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        string last_nam = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_nam}, {first_nam} {last_nam}.");
     }
 }
