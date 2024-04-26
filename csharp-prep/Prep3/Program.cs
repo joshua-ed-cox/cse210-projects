5f22ceffe3ad454ff6651b88d87398008501d06a
joshua-ed-cox <jec.professions@gmail.com>
2024-04-26 05:11:48 -0600

    finished prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // variable initialization
+        int userGuess = 0;
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("Welocome to the magic number game!");
+
+        while (userGuess != randomNumber)
+        {
+            Console.Write("Please enter a guess 1-100: ");
+            string userGuessInput = Console.ReadLine();
+            userGuess = int.Parse(userGuessInput);
+
+
+            if (userGuess > randomNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("The magic number is lower");
+            }
+            else if (userGuess < randomNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("The magic number is higher");
+            }
+        }
+        Console.WriteLine("");
+        Console.WriteLine("You guessed the magic nubmer!");
     }
 }
