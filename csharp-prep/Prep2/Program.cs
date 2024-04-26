f3a6e073f9ba5d8fda65026a79d07bb3e380a37f
joshua-ed-cox <jec.professions@gmail.com>
2024-04-26 03:24:25 -0600

    finished the prep two assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter you grade percentage: ");
+        string userInput = Console.ReadLine();
+
+        int userPercentage = int.Parse(userInput);
+        string userGrade = "";
+        bool didPass;
+        
+        if (userPercentage >= 90)
+        {
+            userGrade = "A";
+            didPass = true;
+        }
+        else if (userPercentage >= 80)
+        {
+            userGrade = "B";
+            didPass = true;
+        }
+        else if (userPercentage >= 70)
+        {
+            userGrade = "C";
+            didPass = true;
+        }
+        else
+        {
+            didPass = false;
+        }
+
+        if (didPass)
+        {
+            Console.WriteLine($"You passed the class with a grade of: {userGrade}");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class");
+        }
+
     }
 }
