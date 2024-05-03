c24004862afa448752f1ca30cfbd2cd5e547588b
joshua-ed-cox <jec.professions@gmail.com>
2024-05-03 03:06:40 -0600

    finished C# prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input = 1;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished");
+
+        
+        while (input != 0)
+        {
+            Console.Write("Enter Number: ");
+            input = int.Parse(Console.ReadLine());
+            numbers.Add(input);
+        }
+
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int max = numbers.Max();
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered");
+        }
     }
 }
